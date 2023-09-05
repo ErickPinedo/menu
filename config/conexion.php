4da851f94b2e796ea579d9357c052688b83a37dc
Erick Pinedo <pinedoerick76@gmail.com>
2023-09-04 22:32:08 -0600

    agregue nombre de la db

--- config/conexion.php
+++ config/conexion.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // Dirección del servidor de la base de datos
 $username = "root"; // Nombre de usuario de MySQL
 $password = ""; // Contraseña de MySQL
-$dbname = ""; // Nombre de la base de datos
+$dbname = "menu"; // Nombre de la base de datos
 
 // Crear una conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
